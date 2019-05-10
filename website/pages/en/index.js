92309673bfeb4916a52c057e82fa658a6e8122c8
Pawel Kowaluk <pensjonatus@gmail.com>
2019-05-10 16:17:34 +0200

    update info about the hosts

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -97,7 +97,7 @@ class Index extends React.Component {
         className="productShowcaseSection paddingBottom"
         style={{textAlign: 'center'}}>
         <h2>Feature Callout</h2>
-        <MarkdownBlock>These are features of this project</MarkdownBlock>
+        <MarkdownBlock>od kilku lat zajmują się zawodowo dokumentacją do oprogramowania. Właściwie od samego początku, techniczna strona tworzenia dokumentacji była dla nich najbardziej interesująca. Dlatego uwielbiają brudzić sobie ręce pisząc skrypty, konfigurując narzędzia i wymyślając rozwiązania związane z automatyzacją, które pozwalają Tech Writerom skupić się na tym co ważne. Na co dzień zastanawiają się jak świat dokumentacji może czerpać korzyści z technologii, których używa się w świecie IT.</MarkdownBlock>
       </div>
     );
 
@@ -150,13 +150,13 @@ class Index extends React.Component {
         {[
           {
             content: 'This is the content of my feature',
-            image: `${baseUrl}img/undraw_react.svg`,
+            image: `https://scontent-frx5-1.xx.fbcdn.net/v/t31.0-8/278281_2223337990898_6169616_o.jpg?_nc_cat=101&_nc_ht=scontent-frx5-1.xx&oh=210032a119a5cb130e704bcab0056be2&oe=5D2B011F`,
             imageAlign: 'top',
             title: 'Michał',
           },
           {
             content: 'The content of my second feature',
-            image: `${baseUrl}img/undraw_operating_system.svg`,
+            image: `https://avatars3.githubusercontent.com/u/6593513?s=460&v=4`,
             imageAlign: 'top',
             title: 'Paweł',
           },
