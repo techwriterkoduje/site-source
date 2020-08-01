aae841d62891d72538e972f15f9522746d5c4d10
Pawel Kowaluk <pensjonatus@gmail.com>
2020-08-01 15:45:10 +0200

    add cookie consent

---
+++ website/src/theme/Footer/index.js
@@ -0,0 +1,142 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
+import CookieConsent from "react-cookie-consent";
+
+function FooterLink({ to, href, label, prependBaseUrlToHref, ...props }) {
+  const toUrl = useBaseUrl(to);
+  const normalizedHref = useBaseUrl(href, {
+    forcePrependBaseUrl: true,
+  });
+  return (
+    <Link
+      className="footer__link-item"
+      {...(href
+        ? {
+            target: "_blank",
+            rel: "noopener noreferrer",
+            href: prependBaseUrlToHref ? normalizedHref : href,
+          }
+        : {
+            to: toUrl,
+          })}
+      {...props}
+    >
+      {label}
+    </Link>
+  );
+}
+
+const FooterLogo = ({ url, alt }) => (
+  <img className="footer__logo" alt={alt} src={url} />
+);
+
+function Footer() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  const { themeConfig = {} } = siteConfig;
+  const { footer } = themeConfig;
+  const { copyright, links = [], logo = {} } = footer || {};
+  const logoUrl = useBaseUrl(logo.src);
+
+  if (!footer) {
+    return null;
+  }
+
+  return (
+    <footer
+      className={clsx("footer", {
+        "footer--dark": footer.style === "dark",
+      })}
+    >
+      <div className="container">
+        {links && links.length > 0 && (
+          <div className="row footer__links">
+            {links.map((linkItem, i) => (
+              <div key={i} className="col footer__col">
+                {linkItem.title != null ? (
+                  <h4 className="footer__title">{linkItem.title}</h4>
+                ) : null}
+                {linkItem.items != null &&
+                Array.isArray(linkItem.items) &&
+                linkItem.items.length > 0 ? (
+                  <ul className="footer__items">
+                    {linkItem.items.map((item, key) =>
+                      item.html ? (
+                        <li
+                          key={key}
+                          className="footer__item" // Developer provided the HTML, so assume it's safe.
+                          // eslint-disable-next-line react/no-danger
+                          dangerouslySetInnerHTML={{
+                            __html: item.html,
+                          }}
+                        />
+                      ) : (
+                        <li key={item.href || item.to} className="footer__item">
+                          <FooterLink {...item} />
+                        </li>
+                      )
+                    )}
+                  </ul>
+                ) : null}
+              </div>
+            ))}
+          </div>
+        )}
+        {(logo || copyright) && (
+          <div className="text--center">
+            {logo && logo.src && (
+              <div className="margin-bottom--sm">
+                {logo.href ? (
+                  <a
+                    href={logo.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.footerLogoLink}
+                  >
+                    <FooterLogo alt={logo.alt} url={logoUrl} />
+                  </a>
+                ) : (
+                  <FooterLogo alt={logo.alt} url={logoUrl} />
+                )}
+              </div>
+            )}
+
+            <div // Developer provided the HTML, so assume it's safe.
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: copyright,
+              }}
+            />
+          </div>
+        )}
+      </div>
+      <script
+        src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js"
+        data-cfasync="false"
+      ></script>
+      <script src="/js/cookie-consent-config.js" data-cfasync="false"></script>
+      <CookieConsent
+        location="bottom"
+        buttonText="Akceptuję"
+        cookieName="myAwesomeCookieName2"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+      >
+        Ta strona używa plików cookies (tzw. "ciasteczka")
+      </CookieConsent>
+    </footer>
+  );
+}
+
+export default Footer;
