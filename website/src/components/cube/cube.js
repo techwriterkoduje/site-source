7362c4abb00e3746bc9a0a8fa06ea1b62e7a2f23
Pawel Kowaluk <pensjonatus@gmail.com>
2020-09-25 10:01:48 +0200

    add sound effect

--- website/src/components/cube/cube.js
+++ website/src/components/cube/cube.js
@@ -1,10 +1,14 @@
 import React from "react";
 import cx from "clsx";
+import useSound from "use-sound";
 import styles from "./styles.module.css";
+import coin from "./sound/coin.wav";
 
 export default function Cube() {
+  const [play] = useSound(coin);
+
   return (
-    <div className={cx(styles.cube)}>
+    <div className={cx(styles.cube)} onClick={play}>
       <div className={cx(styles.cubeFace, styles.cubeFaceFront)} />
       <div className={cx(styles.cubeFace, styles.cubeFaceLeft)} />
       <div className={cx(styles.cubeFace, styles.cubeFaceBottom)} />
