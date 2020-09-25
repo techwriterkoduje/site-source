7cd9d953a193920af827be50b616546873975e8a
Pawel Kowaluk <pensjonatus@gmail.com>
2020-09-25 12:20:08 +0200

    add easter egg

--- website/src/components/cube/cube.js
+++ website/src/components/cube/cube.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import cx from "clsx";
 import useSound from "use-sound";
 import styles from "./styles.module.css";
 import coin from "./sound/coin.wav";
+import oneUp from "./sound/oneUp.wav";
 
 export default function Cube() {
-  const [play] = useSound(coin);
-  function onClick() {
-    play();
-    
+  const coinObject = useRef(null);
+  const [coins, setCoins] = useState(0);
+  const [playCoin] = useSound(coin);
+  const [playOneUp] = useSound(oneUp);
+
+  function giveCoin() {
+    setCoins(coins + 1);
+    coinObject.current.className = styles.coin;
+
+    if (coins === 69) {
+      playOneUp();
+      coinObject.current.className = cx(
+        styles.coin,
+        styles.jumpy,
+        styles.oneUp
+      );
+      setCoins(0);
+    } else {
+      playCoin();
+      coinObject.current.className = cx(styles.coin, styles.jumpy);
+    }
+
+    setTimeout(function () {
+      coinObject.current.className = styles.coin;
+    }, 300);
   }
 
   return (
-    <div className={cx(styles.cube)} onClick={onClick}>
-      <div className={styles.coin} />
+    <div className={cx(styles.cube)} onClick={giveCoin}>
+      <div className={styles.coin} ref={coinObject} />
       <div className={cx(styles.cubeFace, styles.cubeFaceFront)}>
         <span className={styles.cubeText}>?</span>
       </div>
