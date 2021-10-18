aba9063333f2addc717dde448e60d1c7c17f5a6a
Doc Developer <39494509+docdeveloper@users.noreply.github.com>
2021-10-18 10:51:50 +0200

    Add links to other platforms

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -21,6 +21,30 @@ const subscribeLinks = [
     label: 'Player.fm',
   },
   {
+    to: 'https://podcasts.apple.com/us/podcast/tech-writer-koduje/id1463669718?uo=4',
+    label: 'Apple Podcasts',
+  },
+    {
+    to: 'https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy84YWZiYTljL3BvZGNhc3QvcnNz',
+    label: 'Google Podcasts',
+  },
+    {
+    to: 'https://overcast.fm/itunes1463669718/tech-writer-koduje',
+    label: 'Overcast',
+  },
+    {
+    to: 'https://www.breaker.audio/tech-writer-koduje',
+    label: 'Breaker',
+  },
+    {
+    to: 'https://pca.st/H7El',
+    label: 'Pocket Casts',
+  },
+    {
+    to: 'https://radiopublic.com/tech-writer-koduje-6nM7mE',
+    label: 'RadioPublic',
+  },
+  {
     to: 'https://anchor.fm/s/8afba9c/podcast/rss',
     label: 'RSS',
   },
