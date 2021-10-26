a2cf95d4af18dbd2a942caea1c63d407b96bae4b
Pawel Kowaluk <pensjonatus@gmail.com>
2021-10-26 09:56:44 +0200

    latest episode on the home page

--- website/src/components/Latest/Latest.js
+++ website/src/components/Latest/Latest.js
@@ -5,7 +5,7 @@ import styles from './Latest.module.css';
 export default function Latest() {
   return (
     <div className={styles.wrapper}>
-      <AnchorEmbed episodeId="34-Tech-Writer-dokumentuje--testuje--koduje--lokalizuje-i-projektuje--czyli-czowiek-renesansu-w-dokumentacji-e16q3at" />
+      <AnchorEmbed episodeId="35-Tech-Writer-chce-kodowa-wicej-e198b1g" />
     </div>
   );
 }
