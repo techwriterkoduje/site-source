de3fb23b842666fbfece46944e4986b2ff747960
Doc Developer <39494509+docdeveloper@users.noreply.github.com>
2021-10-31 08:24:38 +0100

    Update latest episode link on home page

--- website/src/components/Latest/Latest.js
+++ website/src/components/Latest/Latest.js
@@ -5,7 +5,7 @@ import styles from './Latest.module.css';
 export default function Latest() {
   return (
     <div className={styles.wrapper}>
-      <AnchorEmbed episodeId="35-Tech-Writer-chce-kodowa-wicej-e198b1g" />
+      <AnchorEmbed episodeId="36-Tech-Writer-si-boi--czyli-Halloween-Special-2021-e19ecpv" />
     </div>
   );
 }
