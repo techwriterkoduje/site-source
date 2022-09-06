fdce17b158dc249b5e737fbe2160309db85b4d2d
Doc Developer <39494509+docdeveloper@users.noreply.github.com>
2022-09-06 08:25:54 +0200

    Update episode ID

--- website/src/components/Latest/Latest.js
+++ website/src/components/Latest/Latest.js
@@ -5,7 +5,7 @@ import styles from './Latest.module.css';
 export default function Latest() {
   return (
     <div className={styles.wrapper}>
-      <AnchorEmbed episodeId="44-Rekrutacja-Tech-Writerw-w-praktyce--czyli-prawie-wszystko-co-trzeba-wiedzie-o-rynku-pracy-dla-technoskrybw-w-Polsce-e1ltot4" />
+      <AnchorEmbed episodeId="45-Tech-Writer-wybiera-pomidzy-narzdziami-open-source-a-enterprise--czyli-jak-zbudowa-tech-stack-z-przyszoci-e1ncemn" />
     </div>
   );
 }
